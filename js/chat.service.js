18fe71008d4ddfd545f02f6917d85cc699b57d74
Leto01 <80838862+Leto01@users.noreply.github.com>
2023-04-30 09:10:38 +0200

    api service methods implemented

--- js/chat.service.js
+++ js/chat.service.js
@@ -1,38 +1,57 @@
-const BASE_URL = 'https://www2.hs-esslingen.de/~melcher/map/chat/api/?'
+const BASE_URL = 'https://www2.hs-esslingen.de/~melcher/map/chat/api/'
 const query = "request=test"
-const POST = {method: "POST"};
 const token = "s";
 
-export async function register (userid, password, nickname, fullname) {
-    //TODO
-    query = `userid=${userid}&password=${password}`; 
-    return await fetch(BASE_URL + query, POST);
-}
-
 export async function login (userid, password) {
-    query = `userid=${userid}&password=${password}`; 
-    return await fetch(BASE_URL + query, POST);
+    const body = JSON.stringify({
+        request: 'login',
+        userid: userid,
+        password: password
+      });
+    return await fetch(BASE_URL, body);
 }
 
-export async function logout () {
-    query = `token=${token}`; 
-    return await fetch(BASE_URL + query, POST);
+export async function logout (token) {
+    const body = JSON.stringify({
+        request: 'logout',
+        token: token
+      });
+    return await fetch(BASE_URL, body);
 }
 
-export async function deregister () {
+export async function register (id, pw, nickname, fullname) {
+    const body = JSON.stringify({
+        request: 'register',
+        userid: id,
+        password: pw,
+        nickname: nickname,
+        fullname: fullname
+      });
+    return await fetch(BASE_URL, body);
+}
+
+export async function deregister (token) {
     //TODO
-    query = `userid=${userid}&password=${password}`; 
-    return await fetch(BASE_URL + query, POST);
+    const body = JSON.stringify({
+        request: 'deregister',
+        token: token
+      });
+    return await fetch(BASE_URL, POST);
 }
 
-export async function sendmessage (text) {
-    //TODO
-    query = `userid=${userid}&password=${password}`; 
-    return await fetch(BASE_URL + query, POST);
+export async function sendmessage (msg, token) {
+    const body = JSON.stringify({
+        request: 'sendmessage',
+        token: token,
+        text: msg
+      });
+    return await fetch(BASE_URL, body);
 }
 
-export async function fetchmessage (userid, password) {
-    //TODO
-    query = `userid=${userid}&password=${password}`; 
-    return await fetch(BASE_URL + query);
+export async function fetchmessage (token) {
+    const body = JSON.stringify({
+        request: 'fetchmessages',
+        token: token
+      });
+    return await fetch(BASE_URL, body);
 }
