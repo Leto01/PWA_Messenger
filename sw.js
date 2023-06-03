d3ceac68b1f15458f5be2859da3342ad5d01d0b3
Leto01 <80838862+Leto01@users.noreply.github.com>
2023-06-03 20:45:02 +0200

    x

--- sw.js
+++ sw.js
@@ -1,5 +1,33 @@
+const CACHE_VERSION = {
+  STATIC: "1",
+  DYNAMIC: "1",
+};
+
+const CACHE_LIST = {
+  STATIC_CACHE: `static-pwa-chat-${CACHE_VERSION.STATIC}`,
+  DYNAMIC_CACHE: `dynamic-pwa-chat-${CACHE_VERSION.DYNAMIC}`,
+};
+
+const STATIC_CACHE_ITEMS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/styles.css",
+  "./js/app.js",
+  "./js/chat.js",
+  "./js/chat.service.js",
+  "./js/cookie.service.js",
+  "./js/helper.js",
+  "./js/Login.js",
+  "./js/Register.js",
+];
+
 self.addEventListener("install", (e) => {
-  console.log("install received");
+  e.waitUntil(
+    caches.open(CACHE_LIST.STATIC_CACHE).then((cache) => {
+      return cache.addAll(STATIC_CACHE_ITEMS);
+    })
+  );
 });
 
 self.addEventListener("activate", (e) => {
@@ -7,10 +35,14 @@ self.addEventListener("activate", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
-  if ((e).request.url.indexOf('www2.hs-esslingen.de') > -1) return;
-  if ((e).request.url.indexOf('chrome-extension') > -1) return;
-  if ((e).request.url.indexOf(location.host) == -1) return;
-  if ((e).request.url.indexOf(location.pathname) == -1) return;
-
-  console.log("didn't return D:")
+  if (e.request.url.indexOf("www2.hs-esslingen.de") > -1) return;
+  if (e.request.url.indexOf("chrome-extension") > -1) return;
+  if (e.request.url.indexOf(location.host) == -1) return;
+  if (e.request.url.indexOf(location.pathname) == -1) return;
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      console.log(e.request)
+      return res || fetch(e.request);
+    })
+  )
 });
