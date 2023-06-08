5ca6f7ae09cd21a9a0255cd135d5c3676b3ca46c
Leto01 <80838862+Leto01@users.noreply.github.com>
2023-06-08 21:14:31 +0200

    small changes for caching

--- sw.js
+++ sw.js
@@ -24,10 +24,11 @@ const STATIC_CACHE_ITEMS = [
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_LIST.STATIC_CACHE).then((cache) => {
+    caches.open(CACHE_LIST.STATIC_CACHE).then(cache => {
       return cache.addAll(STATIC_CACHE_ITEMS);
     })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
@@ -35,13 +36,15 @@ self.addEventListener("activate", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
+  console.log(e.request)
+  console.log("Fetching detected");
   if (e.request.url.indexOf("www2.hs-esslingen.de") > -1) return;
   if (e.request.url.indexOf("chrome-extension") > -1) return;
   if (e.request.url.indexOf(location.host) == -1) return;
   if (e.request.url.indexOf(location.pathname) == -1) return;
   e.respondWith(
     caches.match(e.request).then(res => {
-      console.log(e.request)
+      console.log(res);
       return res || fetch(e.request);
     })
   )
