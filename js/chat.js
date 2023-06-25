9372e3b16b429c34d3a06c708e19f6c55968d83e
leto.ziegler <leto.ziegler@quanto-solutions.de>
2023-06-25 11:53:45 +0200

    helplink update

--- js/chat.js
+++ js/chat.js
@@ -164,7 +164,7 @@ function createDropdownContent() {
 }
 
 function openHelpPage(){
-  const helpUrl = "https://github.com/Leto01/PWA_Messenger/blob/master/README.md#-help";
+  const helpUrl = "https://github.com/Leto01/PWA_Messenger#-help";
   window.open(helpUrl, "_blank");
 }
 
